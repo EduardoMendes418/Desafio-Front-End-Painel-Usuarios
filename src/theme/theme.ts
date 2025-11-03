bbda0ffd0c9722c28f190cf7891d9ca0e548fcc1
eduardo <eduardomendes418@gmail.com>
2025-11-03 13:56:24 -0300

    [FPU-12] feat -  create theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material/styles';
+
+export const getTheme = (darkMode: boolean) =>
+  createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#1976d2',
+      },
+      secondary: {
+        main: '#dc004e',
+      },
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            textTransform: 'none',
+          },
+        },
+      },
+    },
+  });
